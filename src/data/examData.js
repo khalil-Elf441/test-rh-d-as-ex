3fcc060dac42884c90e27d61ae54bb186e9727b0
khalil-Elf441 <khalil.elf441@gmail.com>
2025-10-26 23:18:38 +0100

    refacto make it better

--- src/data/examData.js
+++ src/data/examData.js
@@ -1,39 +1,18 @@
+// Dynamically import all exam.json files from mock directories
+const modules = import.meta.glob('./mocks/ex316-*/exam.json', { eager: true });
 
-// Import all mocks and create a map for dynamic access
-import mock1 from './mocks/ex316-mock1.json';
-import mock2 from './mocks/ex316-mock2.json';
-import mock3 from './mocks/ex316-mock3.json';
-import mock4 from './mocks/ex316-mock4.json';
-import mock5 from './mocks/ex316-mock5.json';
-import mock6 from './mocks/ex316-mock6.json';
-import mock7 from './mocks/ex316-mock7.json';
-import mock8 from './mocks/ex316-mock8.json';
-import mock9 from './mocks/ex316-mock9.json';
-import mock10 from './mocks/ex316-mock10.json';
-import mock11 from './mocks/ex316-mock11.json';
-import mock12 from './mocks/ex316-mock12.json';
-import mock13 from './mocks/ex316-mock13.json';
-import mock14 from './mocks/ex316-mock14.json';
-import mock15 from './mocks/ex316-mock15.json';
+const loadedExams = Object.values(modules).map(module => module.default);
 
+// Sort exams by examId to maintain a consistent order
+loadedExams.sort((a, b) => {
+  const numA = parseInt(a.examId.split('-').pop(), 10);
+  const numB = parseInt(b.examId.split('-').pop(), 10);
+  return numA - numB;
+});
 
+export const allExams = loadedExams;
 
-export const examDataMap = {
-  [mock1.examId]: mock1,
-  [mock2.examId]: mock2,
-  [mock3.examId]: mock3,
-  [mock4.examId]: mock4,
-  [mock5.examId]: mock5,
-  [mock6.examId]: mock6,
-  [mock7.examId]: mock7,
-  [mock8.examId]: mock8,
-  [mock9.examId]: mock9,
-  [mock10.examId]: mock10,
-  [mock11.examId]: mock11, 
-  [mock12.examId]: mock12, 
-  [mock13.examId]: mock13, 
-  [mock14.examId]: mock14, 
-  [mock15.examId]: mock15, 
-};
-
-export const allExams = [mock1, mock2, mock3, mock4, mock5, mock6, mock7, mock8, mock9, mock10, mock11, mock12, mock13, mock14, mock15];
+export const examDataMap = allExams.reduce((acc, exam) => {
+  acc[exam.examId] = exam;
+  return acc;
+}, {});
